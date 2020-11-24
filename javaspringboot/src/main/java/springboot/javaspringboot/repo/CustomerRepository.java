15f9814cf24bdebf549010eef5dc0dbb41dcd5b8
Than Htike Soe <than@ThannoMacBook-puro.local>
2020-11-24 15:52:01 +0900

    test demo

---
+++ javaspringboot/src/main/java/springboot/javaspringboot/repo/CustomerRepository.java
@@ -0,0 +1,11 @@
+package springboot.javaspringboot.repo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import  springboot.javaspringboot.model.Customer;
+
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+	List<Customer> findByAge(int age);
+}
